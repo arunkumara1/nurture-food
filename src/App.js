592e791479bb75907ad258f382e23cbe348ec007
arunkumar.a1 <arunkumar.a1>
2020-09-21 11:37:53 +0530

    refs# initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+const app = () =>  {
+    return (
+      <div className="app">
+        <p>Lets build a nurture ffod APP!!</p>
+      </div>
+    );
+}
+
+export default app;
